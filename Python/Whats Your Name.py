b5b427aa82d082a06711a0f7e5323627fb0934fa
Gary Read <garyyread@users.noreply.github.com>
2015-06-20 13:25:00 +0100

    Challlenge complete

---
+++ Python/Whats Your Name.py
@@ -0,0 +1,5 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+first = raw_input()
+last = raw_input()
+
+print("Hello " + first + " " + last + "! You just delved into python.")

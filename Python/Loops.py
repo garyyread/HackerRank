228826a0a14a83f3f2f8b3fb06983ac22ba12957
Gary Read <garyyread@users.noreply.github.com>
2015-06-20 13:21:45 +0100

    Challenge complete
    Read an integer N. For all non-negative integers i<N, print i2. See the
    sample for details.
    
    Input Format
    The first and only line contains N.
    
    Constraints
    1≤N≤20
    
    Output Format
    Print N lines, one corresponding to each i.

---
+++ Python/Loops.py
@@ -0,0 +1,12 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+x = int(raw_input())
+
+#for i in range(0,x):
+#    if i >= 0:
+#        print(pow(i,2))
+
+i=0
+while i < x:
+    if i >= 0:
+        print(pow(i,2))
+    i+=1
